c9803c22bf3d40fb83c9def0e762987eda4797b9
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-12-22 18:58:11 +0530

    expense item and component css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem.js";
 
 function App() {
-  return (<h1>Hello</h1>);
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(){
+    return (
+        <div className="expense-item">
+            <div>December 22, 2022</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$300</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
