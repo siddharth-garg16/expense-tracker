51257817a40de2f2d690cbd9b2ccee33ed44795f
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-12-23 15:17:06 +0530

    data using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import ExpenseItem from "./components/ExpenseItem.js";
 
 function App() {
+  // let expenseDate = new Date(2022, 12, 12);
+  // let expenseAmount = 300;
+  // let expenseTitle = "School Fee";
+  let itemArray = [
+    {
+      id:"e1",
+      title:"school",
+      amount:300,
+      date:new Date(2022, 12, 12)
+    },
+    {
+      id:"e2",
+      title:"insurance",
+      amount:500,
+      date:new Date(2022, 10, 10)
+    },
+    {
+      id:"e3",
+      title:"rent",
+      amount:750,
+      date:new Date(2022, 9, 9)
+    }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem date={itemArray[0].date} title={itemArray[0].title} amount={itemArray[0].amount}></ExpenseItem>
+      <ExpenseItem date={itemArray[1].date} title={itemArray[1].title} amount={itemArray[1].amount}></ExpenseItem>
+      <ExpenseItem date={itemArray[2].date} title={itemArray[2].title} amount={itemArray[2].amount}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
+function ExpenseItem(props){
+    const month = props.date.toLocalString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocalString('en-US', {day: '2-digit'});
     return (
         <div className="expense-item">
-            <div>December 22, 2022</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$300</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
